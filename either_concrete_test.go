937c4d73c4ad84ab46f0662f5aa47fc947ae056a
michael.long <michael.long@deciphernow.com>
2019-02-12 12:47:40 -0500

    Adds generated Either and its unit test

---
+++ either_concrete_test.go
@@ -0,0 +1,51 @@
+package fp
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestStringIntTry(t *testing.T) {
+	var actual = StringIntTry(stringToInt, "12")
+	var expected = IntRight{12}
+	if actual != expected {
+		t.Errorf("StringIntTry  %v, want %v", actual, expected)
+	}
+}
+
+func TestStringIntMapSuccess(t *testing.T) {
+	var actual = StringIntMap(StringRight{"12"}, stringToInt)
+	var expected = IntRight{12}
+	if actual != expected {
+		t.Errorf("StringIntMap had type: %T and value: %v, should have been type:  %T  value: %v ", actual, actual, expected, expected)
+	}
+}
+
+func TestStringIntMapError(t *testing.T) {
+	var actual = StringIntMap(StringRight{"12"}, stringToIntError)
+
+	if reflect.TypeOf(actual).Name() != "Left" {
+		t.Errorf("StringIntMap should have been type: Left  but was type:  %T  value: %v ", actual, actual)
+	}
+}
+
+func TestStringIntFlatMapSuccess(t *testing.T) {
+	var actual = StringIntFlatMap(StringRight{"12"}, stringToIntEither)
+	var expected = IntRight{12}
+	if actual != expected {
+		t.Errorf("StringIntFlatMap had type: %T and value: %v, should have been type:  %T  value: %v ", actual, actual, expected, expected)
+	}
+}
+
+func stringToInt(x string) (error, int) {
+	return nil, 12
+}
+
+func stringToIntError(x string) (error, int) {
+	return errors.New("bull"), -1
+}
+
+func stringToIntEither(x string) interface{} {
+	return IntRight{12}
+}
--- either_generic.go
+++ either_generic.go
@@ -1,26 +1,53 @@
-//go:generate genny -in=either_generic.go -out=either_concrete.go gen  Eith=string,int
+//go:generate genny -in=either_generic.go -out=either_concrete.go gen  "O=string,bool P=int"
 
 package fp
 
 import (
+	"fmt"
+
 	"github.com/cheekybits/genny/generic"
 )
 
-type Eith generic.Type
+type O generic.Type
+type P generic.Type
 
-type EithGeneric struct{}
-
-type EitherContainer interface{}
-
-type EithRight struct {
-	value Eith
+type PRight struct {
+	value P
 }
 
-func (e *EithGeneric) Try2(f func(Eith) (error, Eith), x Eith) EitherContainer {
+type ORight struct {
+	value O
+}
+
+func OPTry(f func(O) (error, P), x O) interface{} {
 	err, r := f(x)
 	if err != nil {
 		return Left{err}
 	} else {
-		return EithRight{r}
+		return PRight{r}
+	}
+}
+
+func OPMap(e interface{}, f func(O) (error, P)) interface{} {
+	switch e.(type) {
+	case Left:
+		return e
+	case ORight:
+		v := e.(ORight).value
+		return OPTry(f, v)
+	default:
+		panic(fmt.Sprintf("Unknown type %T", e))
+	}
+}
+
+func OPFlatMap(e interface{}, f func(O) interface{}) interface{} {
+	switch e.(type) {
+	case Left:
+		return e
+	case ORight:
+		v := e.(ORight).value
+		return f(v)
+	default:
+		panic(fmt.Sprintf("Unknown type %T", e))
 	}
 }
