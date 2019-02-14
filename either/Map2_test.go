246b10282621753c25f31a7288d9809742fa4ca8
michael.long <michael.long@deciphernow.com>
2019-02-14 10:53:39 -0500

    Adds another test around 1st parameter error

--- either/Map2_test.go
+++ either/Map2_test.go
@@ -18,7 +18,19 @@ func TestMap2Success(t *testing.T) {
 	}
 }
 
-func TestMap2Failure(t *testing.T) {
+func TestMap2Failure1st(t *testing.T) {
+	var e2 = IntRight{12}
+	var e1 = Left{errors.New("Dang!")}
+	f := func(a, b int) int {
+		return a + b
+	}
+	actual := Map2(e1, e2, f)
+	if actual != e1 {
+		t.Errorf("StringIntFlatMap had type: %T and value: %v, should have been type:  %T  value: %v ", actual, actual, e1, e1)
+	}
+}
+
+func TestMap2Failure2nd(t *testing.T) {
 	var e1 = IntRight{12}
 	var e2 = Left{errors.New("Dang!")}
 	f := func(a, b int) int {
